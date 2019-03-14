fc3b57dcb98589f1331993014a143b59b14fc47e
Kristen Shea <kstar61@live.com>
2019-03-14 16:13:47 -0500

    .

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -2,11 +2,40 @@
 
 namespace FizzBuzz
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //print number starting from 1 to 100. When a number is multiple of three,
+            //print “Fizz” instead of a number on the console and if multiple of five then
+            //print “Buzz” on the console. For numbers which are multiple of three as well
+            //five, print “FizzBuzz” on the console.
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine($"FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine($"Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine($"Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
         }
     }
+
+
+
+
 }
